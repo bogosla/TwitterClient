82829bba54a43e59874b2b2ad97cc2d73ca419f0
bogosla <jdestine68@gmail.com>
2022-08-04 18:13:21 -0700

    add onScroll and pullRefresh listener

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.databinding.ActivityTimelineBinding;
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -26,8 +28,12 @@ public class TimelineActivity extends AppCompatActivity {
     public static final String TAG = "TimelineActivity";
     ActivityTimelineBinding biding;
     TweetAdapter adapter;
-    List<Tweet> tweets;
+    List<Tweet> tweets = new ArrayList<>();
     RestClient client;
+    RecyclerView rvTweets;
+    SwipeRefreshLayout refreshContainer;
+    int page = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,46 +41,68 @@ public class TimelineActivity extends AppCompatActivity {
         biding = DataBindingUtil.setContentView(this, R.layout.activity_timeline);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
 
-        tweets = new ArrayList<>();
-        RecyclerView rvTweets = biding.rvTweets;
+        rvTweets = biding.rvTweets;
+        refreshContainer = biding.refreshContainer;
 
         adapter = new TweetAdapter(this, tweets);
+
+        // Refresh listener
+        refreshContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                populateTimeLine(1);
+                refreshContainer.setRefreshing(false);
+            }
+        });
+
+        // Scroll listener
         EndlessRecyclerViewScrollListener scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
             @Override
-            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-                // Triggered only when new data needs to be appended to the list
-                // Add whatever code is needed to append new items to the bottom of the list
-                Log.i(TAG, "OnScroll OK");
+            public void onLoadMore(int p, int totalItemsCount, RecyclerView view) {
+                int new_page = page ++;
+                client.getHomeTimeLine(new_page, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        try {
+                            adapter.addAll(Tweet.fromJsonArray(json.jsonArray));
+                        } catch (JSONException e) { e.printStackTrace(); }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Toast.makeText(TimelineActivity.this, "Maybe no internet connection!!", Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         };
 
+        // Add decoration to custom each item
         rvTweets.addItemDecoration(new ItemDecorationTweet());
-        rvTweets.setLayoutManager(linearLayoutManager);
-        rvTweets.setAdapter(adapter);
+        rvTweets.setLayoutManager(linearLayoutManager); // LayoutManager
+        rvTweets.setAdapter(adapter); // Set adapter
 
         rvTweets.addOnScrollListener(scrollListener);
         client = RestApplication.getRestClient(this);
 
-        populateTimeLine();
+        populateTimeLine(1);
 
     }
 
-    public void populateTimeLine() {
-        client.getHomeTimeLine(new JsonHttpResponseHandler() {
+    public void populateTimeLine(int page) {
+        client.getHomeTimeLine(page, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG, "Requests TIMELINE Success");
                 try {
-                    tweets.addAll(Tweet.fromJsonArray(json.jsonArray));
-                    Log.i(TAG, String.valueOf(tweets.toString()));
-                    adapter.notifyDataSetChanged();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                    adapter.clear();
+                    adapter.addAll(Tweet.fromJsonArray(json.jsonArray));
+                } catch (JSONException e) { e.printStackTrace(); }
             }
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-
+                Log.i(TAG, "Requests Failed " + response.toString());
+                Toast.makeText(TimelineActivity.this, "Maybe no internet connection!!", Toast.LENGTH_LONG).show();
             }
         });
     }
