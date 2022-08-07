ce9c219c1bc8155b1e54b04c0a2c4d31d4ee2458
bogosla <jdestine68@gmail.com>
2022-08-06 19:46:35 -0700

    specify columninfo fields for db

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetDAO.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetDAO.java
@@ -1,2 +1,24 @@
-package com.codepath.apps.restclienttemplate.models;public interface TweetDAO {
+package com.codepath.apps.restclienttemplate.models;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface TweetDAO {
+
+    @Query("SELECT * FROM Tweet WHERE id = :tweetId")
+    Tweet byTweetId(Long tweetId);
+
+    @Query("SELECT * FROM Tweet ORDER BY createdAt")
+    List<Tweet> getRecentTweets();
+
+    // Replace strategy is needed to ensure an update on the table row.  Otherwise the insertion will
+    // fail.
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertTweet(Tweet... tweets);
+
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -1,26 +1,40 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class User {
+    @ColumnInfo
     public String name;
+
+    @ColumnInfo
     public String username;
+
+    @ColumnInfo
+    public Long twitter_id;
+
     public String profileUrl;
 
-    public User(String name, String username, String profileUrl) {
+    public User(String name, String username, Long id, String profileUrl) {
         this.name = name;
         this.username = username;
+        this.twitter_id = id;
         this.profileUrl = profileUrl;
     }
+
     public static User fromJson(JSONObject json) throws JSONException {
         return new User(
                 json.getString("name"),
                 json.getString("screen_name"),
+                json.getLong("id"),
                 json.getString("profile_image_url_https")
         );
     }
 
+    @NonNull
     @Override
     public String toString() {
         return "User{" +
